178e5f22623ca81500c9811a813277d4b8e1e4dd
Nilton Filho <101483210+niltinfilho@users.noreply.github.com>
2025-04-12 02:34:12 -0300

    mostrando as barras de gastos semanal

--- expenses/lib/components/chart.dart
+++ expenses/lib/components/chart.dart
@@ -1,3 +1,4 @@
+import 'package:expenses/components/chart_bar.dart';
 import 'package:expenses/models/transaction.dart';
 import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
@@ -23,20 +24,39 @@ class Chart extends StatelessWidget {
         }
       }
 
-      print(DateFormat.E().format(weekDay)[0]);
-      print(totalSum);
-
       return {'day': DateFormat.E().format(weekDay)[0], 'value': totalSum};
+    });
+  }
+
+  double get _weekTotalValue {
+    return groupedTransactions.fold(0.0, (sum, tr) {
+      return sum + (tr['value'] as double);
     });
   }
 
   @override
   Widget build(BuildContext context) {
     groupedTransactions;
-    return const Card(
+    return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
-      child: Row(children: []),
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children:
+              groupedTransactions.map((tr) {
+                return Flexible(
+                  fit: FlexFit.tight,
+                  child: ChartBar(
+                    label: (tr['day'] as String),
+                    value: (tr['value'] as double),
+                    percentage: (tr['value'] as double) / _weekTotalValue,
+                  ),
+                );
+              }).toList(),
+        ),
+      ),
     );
   }
 }
